0ff12c0b5601348f05adf010f0f86c798a779854
Mark Bain <hello@markbaindesign.com>
2014-05-22 10:03:10 +0200

    ++++

--- single.php
+++ single.php
@@ -1,22 +1,8 @@
-<?php
-/**
- * The Template for displaying all single posts.
- *
- * @package WordPress
- * @subpackage Twenty_Ten
- * @since Twenty Ten 1.0
- */
+<?php get_header(); ?>
 
-get_header(); ?>
-
-		<div id="container">
+		<div id="primary">
 			<div id="content" role="main">
-			<?php
-			/* Run the loop to output the post.
-			 * If you want to overload this in a child theme then include a file
-			 * called loop-single.php and that will be used instead.
-			 */
-			get_template_part( 'loop', 'single' );
+			<?php	get_template_part( 'loop', 'single' );
 			?>
 
 			</div><!-- #content -->
