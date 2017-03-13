d2cc13a4e58790fa8abb6f1cefe8515be7d96a72
markbain <markbaindesign@gmail.com>
2017-03-13 17:17:45 +0100

    layout home cards

--- httpdocs/wp-content/themes/the-round/front-page.php
+++ httpdocs/wp-content/themes/the-round/front-page.php
@@ -9,45 +9,61 @@
 	</div><!-- .container-class .container -->
 </div><!-- #id .section-class .section -->
 
-<div id="" class="section-class section">
+<div id="" class="card section">
 	<div class="container-class container">
 		<div class="content-container">
-			<img src="<?php bloginfo('template_url'); ?>/assets/images/books.png" class="alignright">
+			<header><h2>90% of the money you pay for a book goes directly to the author.</h2></header>
+			<p>Flannel occupy hashtag irony hot chicken. Jean shorts lyft normcore, vegan kale chips meggings lumbersexual freegan succulents messenger bag bushwick.</p>
+		</div><!-- .content-container -->
+		<img src="<?php bloginfo('template_url'); ?>/assets/images/devices.png" class="alignright">  
+	</div><!-- .container-class .container -->
+</div><!-- #id .section-class .section -->
+<div class="clearfix"></div>
+<div id="" class="card section">
+	<div class="container-class container">
+		<img src="<?php bloginfo('template_url'); ?>/assets/images/mortarboard.png" class="alignleft">
+		<div class="content-container">
 			<header><h2>90% of the money you pay for a book goes directly to the author.</h2></header>
 			<p>Flannel occupy hashtag irony hot chicken. Jean shorts lyft normcore, vegan kale chips meggings lumbersexual freegan succulents messenger bag bushwick.</p>
 		</div><!-- .content-container -->   
 	</div><!-- .container-class .container -->
 </div><!-- #id .section-class .section -->
+
 <div class="clearfix"></div>
-<div id="" class="section-class section">
+<div id="" class="card section">
 	<div class="container-class container">
 		<div class="content-container">
-			<img src="<?php bloginfo('template_url'); ?>/assets/images/mortarboard.png" class="alignleft">
-			<header><h3>90% of the money you pay for a book goes directly to the author.</h3></header>
+			<header><h2>90% of the money you pay for a book goes directly to the author.</h2></header>
 			<p>Flannel occupy hashtag irony hot chicken. Jean shorts lyft normcore, vegan kale chips meggings lumbersexual freegan succulents messenger bag bushwick.</p>
-		</div><!-- .content-container -->   
-	</div><!-- .container-class .container -->
-</div><!-- #id .section-class .section -->
-<div class="clearfix"></div>
-<div id="" class="section-class section">
-	<div class="container-class container">
-		<div class="content-container">
-			<img src="<?php bloginfo('template_url'); ?>/assets/images/piggy-bank.png" class="alignright">
-			<header><h3>90% of the money you pay for a book goes directly to the author.</h3></header>
-			<p>Flannel occupy hashtag irony hot chicken. Jean shorts lyft normcore, vegan kale chips meggings lumbersexual freegan succulents messenger bag bushwick.</p>
-		</div><!-- .content-container -->   
+		</div><!-- .content-container -->
+		<img src="<?php bloginfo('template_url'); ?>/assets/images/piggy-bank.png" class="alignright">   
 	</div><!-- .container-class .container -->
 </div><!-- #id .section-class .section -->
 <div class="clearfix"></div>
 
-<div id="" class="section-class section">
+<div id="" class="card section">
 	<div class="container-class container">
-		<div class="content-container">
-			<img src="<?php bloginfo('template_url'); ?>/assets/images/donation.png" class="alignleft">
-			<header><h3>The round pays <strong>90%</strong> of the purchase price <strong>directly to the author</strong>.</h3></header>
-			<p>This turns the traditional publish model on its head.</p>
+		<img src="<?php bloginfo('template_url'); ?>/assets/images/donation.png" class="alignleft">
+		<div class="content-container">			
+			<header><h2>The round pays <strong>90%</strong> of the purchase price <strong>directly to the author</strong>.</h2></header>
+			<p>This turns the traditional publishing model on its head, allowing authors to spend more time writing great books!</p>
 		</div><!-- .content-container -->   
 	</div><!-- .container-class .container -->
 </div><!-- #id .section-class .section -->
+
+
+<div id="home-cta" class="section">
+	<div class="container">
+		<div class="content-container">
+		<a href="#">
+			<div class="card">
+				<div><img src="<?php bloginfo('template_url'); ?>/assets/images/books_64.png" class="alignright"><h2>Visit the round bookstore </h2></div>
+			</div>
+		</a>
+		</div><!-- .content-container -->   
+	</div><!-- ..container -->
+</div><!-- #home-cta ..section -->
+
+
 		
 <?php get_footer(); ?>
