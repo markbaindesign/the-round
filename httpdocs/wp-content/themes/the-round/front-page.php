aee9e2090ff015b37afe964fd0c1e60deed5ebac
markbain <markbaindesign@gmail.com>
2017-03-13 15:56:51 +0100

    add icons and layout to home

--- httpdocs/wp-content/themes/the-round/front-page.php
+++ httpdocs/wp-content/themes/the-round/front-page.php
@@ -1,19 +1,53 @@
 <?php get_header(); ?>
-<div id="primary">
-	<div id="content" role="main">			
-		<div class="hero clearfix">
-		<?php while ( have_posts() ) : the_post(); ?>			
-			<?php the_content(); ?>			
-		<?php endwhile; ?>
-		</div><!-- .hero -->
-		<div class="latest-blog-posts  clearfix">
-			<?php get_template_part( 'content', 'latest-blog-posts' ); ?>
-		</div><!-- .latest-blog-posts -->		
-		<div class="latest-creatives clearfix">
-			<?php get_template_part( 'content', 'latest-creatives' ); ?>
-		</div><!-- .latest-blogs -->		
 
-	</div>
-</div>
-<?php get_sidebar(); ?>
+<div id="hero" class="section-class section">
+	<div class="container-class container">
+		<div class="content-container">
+			<h1><strong>Great for educators</strong></h1>
+			<h1>Fair for authors</h1>
+		</div><!-- .content-container -->   
+	</div><!-- .container-class .container -->
+</div><!-- #id .section-class .section -->
+
+<div id="" class="section-class section">
+	<div class="container-class container">
+		<div class="content-container">
+			<img src="<?php bloginfo('template_url'); ?>/assets/images/books.png" class="alignright">
+			<header><h2>90% of the money you pay for a book goes directly to the author.</h2></header>
+			<p>Flannel occupy hashtag irony hot chicken. Jean shorts lyft normcore, vegan kale chips meggings lumbersexual freegan succulents messenger bag bushwick.</p>
+		</div><!-- .content-container -->   
+	</div><!-- .container-class .container -->
+</div><!-- #id .section-class .section -->
+<div class="clearfix"></div>
+<div id="" class="section-class section">
+	<div class="container-class container">
+		<div class="content-container">
+			<img src="<?php bloginfo('template_url'); ?>/assets/images/mortarboard.png" class="alignleft">
+			<header><h3>90% of the money you pay for a book goes directly to the author.</h3></header>
+			<p>Flannel occupy hashtag irony hot chicken. Jean shorts lyft normcore, vegan kale chips meggings lumbersexual freegan succulents messenger bag bushwick.</p>
+		</div><!-- .content-container -->   
+	</div><!-- .container-class .container -->
+</div><!-- #id .section-class .section -->
+<div class="clearfix"></div>
+<div id="" class="section-class section">
+	<div class="container-class container">
+		<div class="content-container">
+			<img src="<?php bloginfo('template_url'); ?>/assets/images/piggy-bank.png" class="alignright">
+			<header><h3>90% of the money you pay for a book goes directly to the author.</h3></header>
+			<p>Flannel occupy hashtag irony hot chicken. Jean shorts lyft normcore, vegan kale chips meggings lumbersexual freegan succulents messenger bag bushwick.</p>
+		</div><!-- .content-container -->   
+	</div><!-- .container-class .container -->
+</div><!-- #id .section-class .section -->
+<div class="clearfix"></div>
+
+<div id="" class="section-class section">
+	<div class="container-class container">
+		<div class="content-container">
+			<img src="<?php bloginfo('template_url'); ?>/assets/images/donation.png" class="alignleft">
+			<header><h3>The round pays <strong>90%</strong> of the purchase price <strong>directly to the author</strong>.</h3></header>
+			<p>This turns the traditional publish model on its head.</p>
+		</div><!-- .content-container -->   
+	</div><!-- .container-class .container -->
+</div><!-- #id .section-class .section -->
+		
 <?php get_footer(); ?>
