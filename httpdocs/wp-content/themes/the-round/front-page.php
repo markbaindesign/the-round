0824a638efc0d30cf62c6d5099ed013bc950a702
markbain <markbaindesign@gmail.com>
2017-03-15 17:16:53 +0100

    adjust home layout for mobile

--- httpdocs/wp-content/themes/the-round/front-page.php
+++ httpdocs/wp-content/themes/the-round/front-page.php
@@ -9,12 +9,12 @@
 	</div><!-- .container-class .container -->
 </div><!-- #id .section-class .section -->
 
-<div id="intro" class="section">
+<div id="intro" class="card section">
 	<div class="container-class container">
 		<div class="content-container">
 			<header><h2>Welcome to <strong>the round</strong>, a collection of great books for English language teachers.</h2></header>
 			<p>All the products at this site are completely independently published and unbelievably reasonably priced.</p>
-			<span class="read-more"><a href="<?php bloginfo('url' ); ?>/creatives">See our titles</a></span>
+			<div class="read-more"><a href="<?php bloginfo('url' ); ?>/resources" class="cta">See our titles</a></div>
 		</div><!-- .content-container -->
 		<img src="<?php bloginfo('template_url'); ?>/assets/images/books.png" class="alignright">
 	</div><!-- .container-class .container -->
@@ -27,7 +27,7 @@
 		<div class="content-container">
 			<header><h2>The round writes methodology books and activity books for English language teachers.</h2></header>
 			<p>Our products are mostly e-books, you can read them in a variety of formats and they are easy to buy. Some of us sell print versions of our books as well. </p>
-			<span class="read-more"><a href="<?php bloginfo('url' ); ?>/creatives">See our titles</a></span>
+			<div class="read-more"><a href="<?php bloginfo('url' ); ?>/resources">See our titles</a></div>
 		</div><!-- .content-container -->   
 	</div><!-- .container-class .container -->
 </div><!-- #id .section-class .section -->
@@ -39,7 +39,7 @@
 		<div class="content-container">
 			<header><h2>All the round's books are extremely reasonably priced</h2></header>
 			<p>Almost all of them are under $10, and quite a few of them are just $1! We don’t think teachers have to pay a lot of money for quality ideas, but we also believe the author should get something too.</p>
-			<span class="read-more"><a href="<?php bloginfo('url' ); ?>/creatives">See our titles</a></span>
+			<div class="read-more"><a href="<?php bloginfo('url' ); ?>/resources">See our titles</a></div>
 		</div><!-- .content-container -->
 		<img src="<?php bloginfo('template_url'); ?>/assets/images/piggy-bank.png" class="alignright">   
 	</div><!-- .container-class .container -->
@@ -58,11 +58,11 @@
 </div><!-- #id .section-class .section -->
 
 
-<div id="" class="section">
+<div id="store-cta" class="section">
 	<div class="container">
 		<div class="content-container">		
 			<div class="content-container">			
-				<header><h2><a href="#">See all the titles</a></h2></header>
+				<header><h2><a href="#" class="cta">See all the titles</a></h2></header>
 			</div><!-- .content-container -->
 		</div><!-- .content-container -->   
 	</div><!-- ..container -->
